d2c300a56027eece8cbb6babfd682a5f7cc3e1c1
Toph Tucker <tophtucker@gmail.com>
2016-09-11 06:13:09 -0400

    custom per-project particle forces working. kinda cool i think hehe

--- forces.js
+++ forces.js
@@ -151,7 +151,7 @@ function forceMotion() {
 
 function forceTorus() {
   var nodes,
-      buffer = 20;
+      buffer = 50;
   function force(alpha) {
     if(isHovering) return;
     for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
@@ -190,6 +190,144 @@ function forceMortality() {
     nodes.forEach(function(d) {
       d.dr = 1-Math.random()*.01;
     });
+  }
+  return force;
+}
+
+function forceGrow() {
+  var nodes;
+  function force(alpha) {
+    for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
+      node = nodes[i];
+      node.r *= node.dr;
+      if(node.r < node.r1) {
+        node.r += (node.r1 - node.r)/500;
+      }
+    }
+  }
+  force.initialize = function(_) {
+    nodes = _;
+    nodes.forEach(function(d) {
+      d.dr = 1-Math.random()*.01;
+    });
+  }
+  return force;
+}
+
+function forceMTA() {
+  var nodes;
+  function force(alpha) {
+    for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
+      node = nodes[i];
+      var theta = Math.atan2(node.vy, node.vx);
+      if(Math.random() > .99) {
+        node.targetTheta += (Math.random() > .5 ? 1 : -1) * Math.floor(Math.random()*3)/2 * Math.PI;
+      }
+      theta = (theta + node.targetTheta) / 2;
+      var target = [
+        node.targetSpeed * Math.cos(theta),
+        node.targetSpeed * Math.sin(theta),
+      ];
+      node.vx += (target[0] - node.vx) / 10;
+      node.vy += (target[1] - node.vy) / 10;
+    }
+  }
+  force.initialize = function(_) {
+    nodes = _;
+    nodes.forEach(function(node) {
+      node.targetTheta = Math.floor(Math.random()*8)/4 * Math.PI;
+      node.targetSpeed = 1;
+    })
+  }
+  return force;
+}
+
+function forceWIC() {
+  var nodes;
+  function force(alpha) {
+    for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
+      node = nodes[i];
+      node.r += 1;
+      if(node.r > innerWidth / 10 || Math.random() > .97) {
+        node.x = Math.round(Math.random() * 5) * innerWidth / 5;
+        node.y = Math.round(Math.random() * 5) * innerWidth / 5;
+        node.r = 0;
+      }
+    }
+  }
+  force.initialize = function(_) {
+    nodes = _;
+  }
+  return force;
+}
+
+function forceETF() {
+  var nodes,
+      extent,
+      x,
+      slope,
+      premium = 1,
+      dpremium = 0;
+  function force(alpha) {
+
+    // Random walk with tiny positive bias (fund growth) & slight drift toward 1:1
+    // premium += premium * dpremium;
+    // dpremium += d3.randomNormal(0, .015)() // random walk
+    //   - (.2 * dpremium) // decelerate
+    //   - (.0005 * (premium - 1)); // tend toward 1; should be nonlinear?
+
+    for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
+      node = nodes[i];
+      node.price += node.price * node.dp;
+      node.dp += (-1 * d3.randomLogNormal()() + 1.7) / 2000 - (.1 * node.dp);
+      node.vx -= (node.x - x(node.price)) / 1000;
+      node.vy -= (node.y - (innerHeight/2)) / 1000;
+    }
+  }
+  force.initialize = function(_) {
+    nodes = _;
+    var priceDistribution = d3.randomNormal(4,.8);
+    nodes.forEach(function(node) {
+      node.price = priceDistribution();
+      node.dp = 0;
+    });
+    extent = d3.extent(nodes, function(d) { return d.price; });
+    x = d3.scaleLinear().domain(extent).range([0,innerWidth]);
+  }
+  return force;
+}
+
+function forceStock() {
+  var nodes;
+  function force(alpha) {
+    for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
+      node = nodes[i];
+
+      var dp = (d3.randomLogNormal(.5,1)() * -1 + 3) / 200;
+      node.vy = -dp * (innerHeight - node.y);
+      node.vx += (2 - node.vx) / 100;
+    }
+  }
+  force.initialize = function(_) {
+    nodes = _;
+  }
+  return force;
+}
+
+function forceISIS() {
+  var nodes;
+  function force(alpha) {
+    for (var i = 0, n = nodes.length, node, k = alpha; i < n; ++i) {
+      node = nodes[i];
+      node.vx += (1 * Math.cos(node.direction) - node.vx) / 10;
+      node.vy += (1 * Math.sin(node.direction) - node.vy) / 10;
+    }
+  }
+  force.initialize = function(_) {
+    nodes = _;
+    nodes.forEach(function(d) {
+      d.direction = Math.floor(Math.random()*4)/2 * Math.PI;
+    })
   }
   return force;
 }
@@ -244,4 +382,11 @@ function distance(a,b) {
   var dx = b[0] - a[0];
   var dy = b[1] - a[1];
   return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+}
+
+function rotateVector(v, theta) {
+  return [
+    v[0] * Math.cos(theta) - v[1] * Math.sin(theta),
+    v[0] * Math.sin(theta) + v[1] * Math.cos(theta)
+  ];
 }
--- projects.js
+++ projects.js
@@ -5,7 +5,20 @@ var projects = [
     url: 'http://www.bloomberg.com/features/2015-stock-chart-trading-game/',
     date: 'October 2015',
     byline: 'managed by Joe Weisenthal and Thomas Houston',
-    description: 'This little mobile-first chart game abuses the very chic aesthetic of the very serious Bloomberg Terminal to (1) waste time and (2) confront the vanity of expecting market-beating returns by assuming the future will look like the past. So the game has an unstated thesis, which it argues just by showing you your results, like what Ian Bogost calls “procedural rhetoric”: the ability of a game, or any system of interaction, to make a point.'
+    description: 'This little mobile-first chart game abuses the very chic aesthetic of the very serious Bloomberg Terminal to (1) waste time and (2) confront the vanity of expecting market-beating returns by assuming the future will look like the past. So the game has an unstated thesis, which it argues just by showing you your results, like what Ian Bogost calls “procedural rhetoric”: the ability of a game, or any system of interaction, to make a point.',
+    trails: 1,
+    colors: function () {
+      return "#" + Math.random().toString(16).slice(2, 8);
+    },
+    radii: function() {
+      return 3;
+    },
+    forces: [
+      ['noise', null],
+      ['mouse', null],
+      ['torus', forceTorus()],
+      ['stock', forceStock()]
+    ]
   },
 
   {
@@ -14,7 +27,18 @@ var projects = [
     url: 'http://www.bloomberg.com/features/2016-etf-files/toy/',
     date: 'March 2016',
     byline: 'research help from Eric Balchunas; managed by Thomas Houston and Joel Weber',
-    description: 'What’s an exchange-traded fund (ETF), and how do people make money on it? For the big banks, the answer turns out to be, in a mathematically precise way: you get paid to balance a big see-saw of weights of various sizes, rolling up and down as prices move. That seems to make it easier to understand. A small child could do it. People go, “That’s it??”, and that feels like a triumph. People intuitively understand complex systems all around them all the time! You just need to code up a rigorous graphical-geometrical-mechanical metaphor.'
+    description: 'What’s an exchange-traded fund (ETF), and how do people make money on it? For the big banks, the answer turns out to be, in a mathematically precise way: you get paid to balance a big see-saw of weights of various sizes, rolling up and down as prices move. That seems to make it easier to understand. A small child could do it. People go, “That’s it??”, and that feels like a triumph. People intuitively understand complex systems all around them all the time! You just need to code up a rigorous graphical-geometrical-mechanical metaphor.',
+    colors: function() {
+      var c = ['#fb8e24','#24FBBA','#FE45A7','#3954AE','#FADA84','#849FFA','#FBB19E','#be10fe','#C3FBD9','#B10C64'];
+      return c[Math.floor(Math.random() * c.length)];
+    },
+    radii: function() { return Math.sqrt(d3.randomLogNormal(7,1.3)()); },
+    forces: [
+      ['noise', null],
+      ['mouse', null],
+      ['torus', forceTorus()],
+      ['etf', forceETF()]
+    ]
   },
 
   {
@@ -23,18 +47,74 @@ var projects = [
     url: 'http://bloomberg.com/whatiscode',
     date: '',
     byline: 'with Paul Ford, Steph Davidson, Adam Pearce; managed and edited by Josh Tyrangiel, Joshua Topolsky, Jim Aley; so many others',
-    description: ''
+    description: '',
+    trails: 1,
+    colors: function() {
+      var c = ['#2800d7','#6381ff','#c63eff','#fc99ff','#00c770','#91f29b','#f94600'];
+      return c[Math.floor(Math.random() * c.length)];
+    },
+    forces: [
+      ['noise', null],
+      ['mouse', null],
+      ['torus', forceTorus()],
+      ['wic', forceWIC()]
+    ]
   },
 
   {
     title: 'The Design Issue',
     slug: 'the-design-issue',
-    byline: 'with Tracy Ma; managed by Thomas Houston'
+    byline: 'with Tracy Ma; managed by Thomas Houston',
+    description: '...',
+    colors: function() {
+      var c = ['#00C0F2', '#EF69A6', '#007E49', '#A88F9D', '#706CB1', '#5FE1FF', '#DC8955', '#0071BB', '#AAC1CE', '#C04843', '#FFF100', '#F16322', '#F8CADE', '#BBD531', '#817E82', '#528F8B', '#7895A5', '#EC1C24'];
+      return c[Math.floor(Math.random() * c.length)];
+    },
+    radii: function() {
+      return 50 + Math.random() * 200;
+    },
+    forces: [
+      ['torus', forceTorus()],
+      ['mouse', null],
+      ['noise', forceNoise().strength(.01).random(2).chaos(10).scale(300).offset(-1).mouse(true)]
+    ]
+  },
+
+  {
+    title: 'The Banality of Islamic State',
+    slug: 'isis',
+    byline: 'managed by Bryant Urstadt; story by Cam Simpson',
+    trails: 1,
+    colors: function() { return 'black'; },
+    radii: function() { return 2; },
+    forces: [
+      ['torus', forceTorus()],
+      ['isis', forceISIS()],
+      ['noise', null],
+      ['mouse', null]
+    ]
   },
 
   {
     title: 'MTA spaghetti',
-    description: 'stuff'
+    slug: 'mta-spaghetti',
+    url: 'http://bl.ocks.org/tophtucker/raw/81c467db46a37dfaf75cacb70552b818',
+    date: 'June 2016',
+    description: 'stuff',
+    trails: 1,
+    colors: function() {
+      var c = ['#0039A6','#009B3A','#939598','#955214','#B634BB','#E00034','#FECB00','#FF6319'];
+      return c[Math.floor(Math.random() * c.length)];
+    },
+    radii: function() {
+      return 10;
+    },
+    forces: [
+      ['noise', null],
+      ['mouse', null],
+      ['torus', forceTorus()],
+      ['mta', forceMTA()]
+    ]
   },
 
   {
@@ -57,7 +137,10 @@ var projects = [
   },
 
   {
-    title: 'The Bowdoin Orient'
+    title: 'The Bowdoin Orient',
+    slug: 'the-bowdoin-orient',
+    url: 'http://bowdoinorient.com',
+    description: 'Wrote “full-stack” PHP/Javascript CMS for college paper. Full-stack including writing and some copy editing. '
   },
 
   {
